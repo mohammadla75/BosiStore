fb6c744b28605eae43a87f00149f3394eff33eb4
mohammadla75 <mlashkari75@gmail.com>
2026-05-23 17:25:10 +0330

    Initial Commit

---
+++ LuxuryStore/LuxuryStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuxuryStoreApp.swift
+//  LuxuryStore
+//
+//  Created by MAC on 3/2/1405 AP.
+//
+
+import SwiftUI
+
+@main
+struct LuxuryStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
